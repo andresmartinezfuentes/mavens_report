1c79665cc126858644d573660826438b3fd809ff
andresmartinezfuentes <202110236@alu.comillas.edu>
2022-12-02 17:25:01 +0100

    main

--- mavens_report_code/mave_report_pdf/mavens_xml_to_pdf.py
+++ mavens_report_code/mave_report_pdf/mavens_xml_to_pdf.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import datetime
 from xhtml2pdf import pisa
+from copy import deepcopy
 
 def extract():
     df_details = pd.read_csv('unformatted_order_details.csv',sep=';')
     df_orders = pd.read_csv('unformatted_orders.csv',sep=';')
     df_orders = df_orders.drop(df_orders.loc[pd.isna(df_orders['date'])].index)
+    df_pizzas = pd.read_csv('pizzas.csv')
     df_types = pd.read_csv('pizza_types.csv',sep=',')
-    return df_details, df_orders, df_types  
+    return df_details, df_orders, df_types,df_pizzas
 
 
-def transform(df_details, df_orders,df_types):
+def transform(df_details, df_orders,df_types,df_pizzas):
     lista = []
     for i in list(df_orders.index):
         try:
@@ -211,8 +213,53 @@ def transform(df_details, df_orders,df_types):
         if j%3 == 0 and j != 0:
             i_sem_total.append(i_sem)    
         j += 1
+
+
+    pizzas = []
+    for j in range(1,53):
+        p = []
+        for i in list(df_selec.index):
+            if df_selec['sem'][i] == j:
+                for k in range(int(df_selec['quantity'][i])):
+                    p.append(df_selec['pizza_id'][i])
+        sm = []
+        for tam in p:
+            sm.append(tam)
+        pizzas.append(sm)
+
+
+    list_count = []
+    pi = ['bbq_ckn_s','bbq_ckn_m','bbq_ckn_l','cali_ckn_s','cali_ckn_m','cali_ckn_l','ckn_alfredo_s','ckn_alfredo_m','ckn_alfredo_l','ckn_pesto_s','ckn_pesto_m','ckn_pesto_l','southw_ckn_s','southw_ckn_m','southw_ckn_l','thai_ckn_s','thai_ckn_m','thai_ckn_l','big_meat_s','big_meat_m','big_meat_l','classic_dlx_s','classic_dlx_m','classic_dlx_l','hawaiian_s','hawaiian_m','hawaiian_l','ital_cpcllo_s','ital_cpcllo_m','ital_cpcllo_l','napolitana_s','napolitana_m','napolitana_l','pep_msh_pep_s','pep_msh_pep_m','pep_msh_pep_l','pepperoni_s','pepperoni_m','pepperoni_l','the_greek_s','the_greek_m','the_greek_l','the_greek_xl','the_greek_xxl','brie_carre_s','calabrese_s','calabrese_m','calabrese_l','ital_supr_s','ital_supr_m','ital_supr_l','peppr_salami_s','peppr_salami_m','peppr_salami_l','prsc_argla_s','prsc_argla_m','prsc_argla_l','sicilian_s','sicilian_m','sicilian_l','soppressata_s','soppressata_m','soppressata_l','spicy_ital_s','spicy_ital_m','spicy_ital_l','spinach_supr_s','spinach_supr_m','spinach_supr_l','five_cheese_s','five_cheese_m','five_cheese_l','four_cheese_s','four_cheese_m','four_cheese_l','green_garden_s','green_garden_m','green_garden_l','ital_veggie_s','ital_veggie_m','ital_veggie_l','mediterraneo_s','mediterraneo_m','mediterraneo_l','mexicana_s','mexicana_m','mexicana_l','spin_pesto_s','spin_pesto_m','spin_pesto_l','spinach_fet_s','spinach_fet_m','spinach_fet_l','veggie_veg_s','veggie_veg_m','veggie_veg_l']
+
+    for p in pizzas:
+        p_count = {}
+        for c in pi:
+            try:
+                p_count[c] = p.count(c)
+            except:
+                p_count[c] = 0
+        list_count.append(p_count)
+
+
+    cost = {}
+    for p in pi:
+        if p != 'Total':
+            cost[p]=list(df_pizzas.loc[df_pizzas.pizza_id == p].values)[0][3]
+
+    list_price = []
+    pi = pi[:-1]
+    for p in pizzas:
+        p_price = {}
+        for c in pi:
+            try:
+                p_price[c] = p.count(c)*cost[c]
+            except:
+                p_price[c] = 0
+        list_price.append(p_price)
+    
         
-    return i_sem_total
+    return i_sem_total, list_count, list_price
+
 
 
 def tipologia():
@@ -229,17 +276,26 @@ def tipologia():
 
     return text
 
-def load_html_to_pdf(i_sem_total):
+def load_html_to_pdf(i_sem_total, list_count, list_price):
     h = ['<html>','<head>','<h2>','<p>','<body>','<h1>','<h3>']
     f = ['</html>','</head>','</h2>','</p>','</body>','</h1>','</h3>']
     html_text = ''
     html_text += h[0] + h[1] + h[5] + 'Report_mavens' + f[5]+ f[1]
     html_text += h[4] + h[2] + 'Tipología de datos' + f[2]
     html_text += h[3] + tipologia() + f[3]
+    html_text += h[2] + 'Reporte de pizzas vendidas por semana' + f[2]
+    for sem in range(len(list_count)):
+        html_text += h[6] + f'Semana {sem+1}' + f[6]
+        html_text += h[3] + str(list_count[sem]) + f[3]
+    html_text += h[2] + 'Reporte de ingresos por semana' + f[2]
+    for sem in range(len(list_price)):
+        html_text += h[6] + f'Semana {sem+1}' + f[6]
+        html_text += h[3] + str(list_price[sem]) + f[3]
     html_text += h[2] + 'Reporte de stock usado por semana' + f[2]
     for sem in range(len(i_sem_total)):
         html_text += h[6] + f'Semana {sem+1}' + f[6]
         html_text += h[3] + str(i_sem_total[sem]) + f[3]
+
     html_text += f[4] + f[0]
 
     # añadir una gráfica
@@ -251,9 +307,8 @@ def load_html_to_pdf(i_sem_total):
     
 
 if __name__=="__main__":
-    with open('report_mavens.xml', 'r', encoding='utf_8') as file:
-        text = file.read()
-
-    df_details, df_orders, df_types = extract()
-    i_sem_total = transform(df_details, df_orders, df_types)
-    load_html_to_pdf(i_sem_total)
+    print('Generando reporte, por favor espere')
+    df_details, df_orders, df_types,df_pizzas = extract()
+    i_sem_total, list_count, list_price = transform(df_details, df_orders, df_types, df_pizzas)
+    load_html_to_pdf(i_sem_total, list_count, list_price)
+    print('Reporte finalizado')
